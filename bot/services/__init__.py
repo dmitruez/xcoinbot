0f362c3281da7a7e8ceb4896301aae2c54db504a
dmitruez <dyezerski@icloud.com>
2025-07-25 17:24:08 +0300

    Пока убираем крипто-обменник

--- bot/services/__init__.py
+++ bot/services/__init__.py
@@ -3,7 +3,6 @@ from aiogram import Bot
 from .admin_service import AdminService
 from .captcha_service import CaptchaService
 from .channel_service import ChannelService
-from .exchange_service import ExchangeService
 from .notifier_service import NotificationService
 from .subscriber_service import SubscriptionService
 from .user_service import UserService
@@ -20,7 +19,6 @@ class Services:
 		self.subscriber: SubscriptionService = SubscriptionService(bot, repos.user, repos.channel)
 		self.user: UserService = UserService(repos.user, admin_repo=repos.admin)
 		self.admin: AdminService = AdminService(repos.admin, repos.user, repos.channel)
-		self.exchange: ExchangeService = ExchangeService()
 
 
 def setup_services(bot: Bot, repos: Repositories) -> Services:
